8e2245e8834c31844d66c8cf13ecead510f79152
gvozdarev-sa <gvozdarev.sa@gmail.com>
2013-09-27 16:46:01 +0400

    Task 3

--- openmp/src/z3.cpp
+++ openmp/src/z3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include <sys/time.h>
 #include <omp.h>
 
@@ -32,11 +33,13 @@ int main(int argc, char *argv[])
     {
         std::cout << "Please specify image file" << std::endl;
     }
-    Image cam( argv[ 1]);
+
+    Image I1( argv[ 1]),
+          I2( argv[ 1]);
 
 
-    int w = cam.GetWidth( ),
-        h = cam.GetHeight( );
+    int w = I1.GetWidth( ),
+        h = I2.GetHeight( );
 
 
 
@@ -44,22 +47,64 @@ int main(int argc, char *argv[])
     timer.Start( );
     #pragma omp parallel
     {
+        #pragma omp segments
+        {
+            #pragma omp segment
+            #pragma omp for schedule(guided, 100)
+            for ( int i = 0; i < h; i++)
+            {
+                for ( int j = 0; j < w; j++)
+                {
+                    Color col1 = I1.GetPixel( i, j  ),
+                          col2 = I1.GetPixel( i, j+1);
+                    Color ncol;
+
+                    ncol.r = std::abs( col1.r - col2.r);
+                    ncol.g = std::abs( col1.g - col2.g);
+                    ncol.b = std::abs( col1.b - col2.r);
+
+                    I1.SetPixel( i, j, ncol);
+                }
+            }
+            #pragma omp segment
+            #pragma omp for schedule(guided, 100)
+            for ( int j = 0; j < w; j++)
+            {
+                for ( int i = 0; i < h; i++)
+                {
+                    Color col1 = I2.GetPixel( i  , j),
+                          col2 = I2.GetPixel( i+1, j);
+                    Color ncol;
+
+                    ncol.r = std::abs( col1.r - col2.r);
+                    ncol.g = std::abs( col1.g - col2.g);
+                    ncol.b = std::abs( col1.b - col2.r);
+
+                    I2.SetPixel( i, j, ncol);
+                }
+            }
+        }
         #pragma omp for schedule(guided, 100)
-        for ( int i = 4; i < h; i++)
+        for ( int i = 0; i < h; i++)
         {
             for ( int j = 0; j < w; j++)
             {
-                Color col = cam.GetPixel( i,j);
+                Color col1 = I1.GetPixel( i, j),
+                      col2 = I2.GetPixel( i, j);
                 Color ncol;
 
-                ncol.r = ncol.g = ncol.b = 0.299f * col.r + 0.587f * col.g + 0.114f * col.b;
-                cam.SetPixel( i, j, ncol);
+                ncol.r = 0.5 * col1.r + 0.5 * col2.r;
+                ncol.g = 0.5 * col1.g + 0.5 * col2.g;
+                ncol.b = 0.5 * col1.b + 0.5 * col2.b;
+
+                I1.SetPixel( i, j, ncol);
             }
         }
     }
+
     timer.Stop( );
 
-    cam.SaveImage( "2.bmp");
+    I1.SaveImage( "2.jpg");
 
     std::cout << "Time : " << timer.GetTime( ) << std::endl;
 
